85d6383a44d90728cd8b9671afb31cf82404ac31
grze2000 <programista333@gmail.com>
2021-03-07 23:01:00 +0100

    price calculation

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,22 @@ const style = document.createElement('style');
 const progressColor = 'hsl(174, 77%, 80%)';
 const backgroundColor = 'hsl(224, 65%, 95%)';
 const slider = document.querySelector('.slider');
+const views = document.querySelector('.views-value');
+const price = document.querySelector('.price-value');
+const billing = document.querySelector('.switch__checkbox');
 
 style.innerHTML = `.slider::-webkit-slider-runnable-track { background: linear-gradient(to right, ${progressColor} 0%, ${progressColor} 50%, ${backgroundColor} 50%, ${backgroundColor} 100%); }`;
 document.querySelector('head').appendChild(style);
 
+const updatePrice = () => {
+  price.innerText = `$${(slider.value*0.16*(billing.checked ? 0.75 : 1)).toFixed(2)}`;
+}
+
 slider.addEventListener('input', function() {
-  style.innerHTML = `.slider::-webkit-slider-runnable-track { background: linear-gradient(to right, ${progressColor} 0%, ${progressColor} ${this.value}%, ${backgroundColor} ${this.value}%, ${backgroundColor} 100%); }`;
+  const val = (this.value - this.min) / (this.max - this.min) * 100;
+  style.innerHTML = `.slider::-webkit-slider-runnable-track { background: linear-gradient(to right, ${progressColor} 0%, ${progressColor} ${val}%, ${backgroundColor} ${val}%, ${backgroundColor} 100%); }`;
+  views.innerText = this.value;
+  updatePrice();
 });
+
+billing.addEventListener('change', updatePrice);
