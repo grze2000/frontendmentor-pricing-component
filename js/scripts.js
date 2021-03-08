22f9aa5029104fb65b3a4a1af4752b36cde7da16
grze2000 <programista333@gmail.com>
2021-03-08 16:06:24 +0100

    fixup! slider styles

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,25 @@ const slider = document.querySelector('.slider');
 const views = document.querySelector('.views-value');
 const price = document.querySelector('.price-value');
 const billing = document.querySelector('.switch__checkbox');
+const steps = {
+  '10K': 8,
+  '50K': 12,
+  '100K': 16,
+  '500K': 24,
+  '1M': 36
+};
 
 style.innerHTML = `.slider::-webkit-slider-runnable-track { background: linear-gradient(to right, ${progressColor} 0%, ${progressColor} 50%, ${backgroundColor} 50%, ${backgroundColor} 100%); }`;
 document.querySelector('head').appendChild(style);
 
 const updatePrice = () => {
-  price.innerText = `$${(slider.value*0.16*(billing.checked ? 0.75 : 1)).toFixed(2)}`;
+  price.innerText = `$${(steps[views.innerText]*(billing.checked ? 0.75 : 1)).toFixed(2)}`;
 }
 
 slider.addEventListener('input', function() {
   const val = (this.value - this.min) / (this.max - this.min) * 100;
   style.innerHTML = `.slider::-webkit-slider-runnable-track { background: linear-gradient(to right, ${progressColor} 0%, ${progressColor} ${val}%, ${backgroundColor} ${val}%, ${backgroundColor} 100%); }`;
-  views.innerText = this.value;
+  views.innerText = Object.keys(steps)[this.value/this.step];
   updatePrice();
 });
 
